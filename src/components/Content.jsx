a8d767852fd47e2436f91f6791bede157d371da2
Nathan D <wibbet@wobbet.com>
2023-02-07 14:05:54 +0000

    add map

---
+++ src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+const Content = () => {
+    return (
+        <div className="content">
+              <h1>Fossil Finder!</h1>
+        </div>
+    )
+}
+
+export default Content
